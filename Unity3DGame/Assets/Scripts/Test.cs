1d6b472cd96f13b744737e1e52f8b98da8110059
xiaomo <xiaomo@xiaomo.info>
2017-08-01 20:15:56 +0800

    添加plugin

--- Unity3DGame/Assets/Scripts/Test.cs
+++ Unity3DGame/Assets/Scripts/Test.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GDGeek;
 
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        StateWithEventMap map = new StateWithEventMap();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
 }
