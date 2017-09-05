3fef4f47321df43b2f766132ed227784654a7f08
xiaomo <xiaomo@xiaomo.info>
2017-09-05 09:34:25 +0800

    rider

--- Unity3DGame/Assets/Scripts/CreateCharacter.cs
+++ Unity3DGame/Assets/Scripts/CreateCharacter.cs
@@ -20,83 +20,13 @@ public class CreateCharacter : MonoBehaviour {
 
     void Start () {
         mRoot = transform;
-        zhanButton = mRoot.Find("zhan").GetComponent<Button>() as Button;
-        faButton = mRoot.Find("fa").GetComponent<Button>() as Button;
-        daoButton = mRoot.Find("dao").GetComponent<Button>() as Button;
         createButton = mRoot.Find("create").GetComponent<Button>() as Button;
         nameInput = mRoot.Find("inputName").GetComponent<InputField>() as InputField;
 
-        zhanButton.onClick.AddListener(ZhanClicked);
-        faButton.onClick.AddListener(FaClicked);
-        daoButton.onClick.AddListener(DaoClicked);
         createButton.onClick.AddListener(CreateClicked);
     }
 
- 
 
-    void Update () {
-		
-	}
-
-
-    /// <summary>
-    /// 战士
-    /// </summary>
-    private void ZhanClicked() {
-        if (zhanObj == null)
-        {
-            GameObject obj = GameManager.getInstance.LoadResources<GameObject>(GameConst.Resource.zhanPath);
-            zhanObj = GameObject.Instantiate(obj);
-            zhanObj.transform.position = Vector3.zero;
-        }
-        else
-        {
-            zhanObj.SetActive(true);
-        }
-
-        GameManager.getInstance.HideGameObject(faObj);
-        GameManager.getInstance.HideGameObject(daoObj);
-    }
-
-    /// <summary>
-    /// 法师
-    /// </summary>
-    private void FaClicked()
-    {
-        if (faObj == null)
-        {
-            GameObject obj = GameManager.getInstance.LoadResources<GameObject>(GameConst.Resource.faPath);
-            faObj = GameObject.Instantiate(obj);
-            faObj.transform.position = Vector3.zero;
-        }
-        else {
-            faObj.SetActive(true);
-        }
-
-        GameManager.getInstance.HideGameObject(zhanObj);
-        GameManager.getInstance.HideGameObject(daoObj);
-
-    }
-
-    /// <summary>
-    /// 道士
-    /// </summary>
-    private void DaoClicked()
-    {
-        if (daoObj == null)
-        {
-            GameObject obj = GameManager.getInstance.LoadResources<GameObject>(GameConst.Resource.faPath);
-            daoObj = GameObject.Instantiate(obj);
-            daoObj.transform.position = Vector3.zero;
-        }
-        else
-        {
-            daoObj.SetActive(true);
-        }
-
-        GameManager.getInstance.HideGameObject(zhanObj);
-        GameManager.getInstance.HideGameObject(faObj);
-    }
 
     /// <summary>
     /// 创角
