388920e29342512e06144b3197729d7318e0e539
hupeng <xiaomo@xiaomo.info>
2018-10-09 15:45:23 +0800

    添加常量类

---
+++ MRCY/Constants.cs
@@ -0,0 +1,13 @@
+namespace MrCy {
+    public static class Constants {
+        public const string Tip = "提示";
+        public const string WeakWarn = "弱警告";
+        public const string Warn = "警告";
+        public const string Danger = "危险";
+        public const string Error = "错误";
+        public const string ExitSystem = "确定退出系统吗？";
+        public const string UserNameOrPasswordError = "用户名或密码错误？";
+        public const string PleaseInputUserName = "请输入用户名";
+        public const string PleaseInputPassword = "请输入密码";
+    }
+}
